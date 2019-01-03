5b91ea89d87da52b41b1d56edaf939e9699ded04
Istvan Acs <istvan.acs89@gmail.com>
2019-01-03 22:24:04 +0000

    Optimize booking

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -14,6 +14,17 @@ const transformEvent = event => {
   };
 };
 
+const transformBooking = booking => {
+  return {
+    ...booking._doc,
+    _id: booking.id,
+    user: user.bind(this, booking._doc.user),
+    event: singleEvent.bind(this, booking._doc.event),
+    createdAt: dateToString(booking._doc.createdAt),
+    updatedAt: dateToString(booking._doc.updatedAt)
+  }
+};
+
 const events = async eventIds => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
@@ -62,14 +73,7 @@ module.exports = {
     try {
       const bookings = await Booking.find();
       return bookings.map(booking => {
-        return {
-          ...booking._doc,
-          _id: booking.id,
-          user: user.bind(this, booking._doc.user),
-          event: singleEvent.bind(this, booking._doc.event),
-          createdAt: dateToString(booking._doc.createdAt),
-          updatedAt: dateToString(booking._doc.updatedAt)
-        };
+        return transformBooking(booking);
       });
     } catch (err) {
       throw err;
@@ -128,14 +132,7 @@ module.exports = {
       event: fetchedEvent
     });
     const result = await booking.save();
-    return {
-      ...result._doc,
-      _id: result.id,
-      user: user.bind(this, booking._doc.user),
-      event: singleEvent.bind(this, booking._doc.event),
-      createdAt: dateToString(result._doc.createdAt),
-      updatedAt: dateToString(result._doc.updatedAt)
-    };
+    return transformBooking(result);
   },
   cancelBooking: async args => {
     try {
