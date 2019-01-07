dc2e7238e10ce268938bc1a9820bfdcfff40dee3
Istvan Acs <istvan.acs89@gmail.com>
2019-01-07 23:59:34 +0000

    Add navigation component to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/navigation/MainNavigation';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter className="App">
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+      <BrowserRouter>
+        <React.Fragment>
+        <MainNavigation />
+          <main>
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
