484a33ec00d255e1dfa2338b8564096bfcf46c3a
Istvan Acs <istvan.acs89@gmail.com>
2019-01-19 15:43:20 +0000

    Add a backdrop to a modal and some functionality to buttons

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import Modal from '../components/modal/Modal';
+import Backdrop from '../components/backdrop/Backdrop';
 
 import './Events.css';
 
 class EventsPage extends Component {
+  state ={
+    creating: false
+  };
+
+  createEventHandler = () => {
+    this.setState({creating: true});
+  };
+
+  modalConfirmHandler = () => {
+    this.setState({creating: false});
+  };
+
+  modalCancelHandler = () => {
+    this.setState({creating: false});
+  };
+
   render () {
     return (
       <React.Fragment>
-        <Modal title="Add Event" canCancel="true" canConfirm="true">
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && <Modal title="Add Event" canCancel="true" canConfirm="true" onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
           <p>Modal content</p>
-        </Modal>
+        </Modal>}
         <div className="events-control">
           <p>Share your own Events!</p>
-          <button className="btn">Create Event</button>
+          <button className="btn" onClick={this.createEventHandler}>Create Event</button>
         </div>
       </React.Fragment>
     );
