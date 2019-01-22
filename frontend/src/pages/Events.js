19ea1a28fea203641a17dcc5595c8f9f2845cdb8
Istvan Acs <istvan.acs89@gmail.com>
2019-01-22 22:49:59 +0000

    Add events WIP

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Modal from '../components/modal/Modal';
 import Backdrop from '../components/backdrop/Backdrop';
-
+import AuthContext from '../context/auth-context';
 import './Events.css';
 
 class EventsPage extends Component {
   state ={
     creating: false
   };
+
+  static contextType = AuthContext;
 
   constructor(props) {
     super(props);
@@ -17,6 +19,7 @@ class EventsPage extends Component {
     this.descriptionElRef = React.createRef();
   }
 
+
   createEventHandler = () => {
     this.setState({creating: true});
   };
@@ -24,16 +27,56 @@ class EventsPage extends Component {
   modalConfirmHandler = () => {
     this.setState({creating: false});
     const title = this.titleElRef.current.value;
-    const price = this.priceElRef.current.value;
+    const price = +this.priceElRef.current.value;
     const date = this.dateElRef.current.value;
     const description = this.descriptionElRef.current.value;
 
-    if(title.trim().length === 0 || price.trim().length === 0 || date.trim().length === 0 || description.trim().length === 0) {
+    if(title.trim().length === 0 || price <= 0 || date.trim().length === 0 || description.trim().length === 0) {
       return;
     }
 
     const event = {title, price, date, description}
     console.log(event);
+
+    const requestBody = {
+        query: `
+            mutation {
+              createEvent(eventInput: {title: "${title}", description: "${description}", price: ${price} ,date: "${date}"}) {
+                _id
+                title
+                description
+                date
+                price
+                creator {
+                  _id
+                  email
+                }
+              }
+            }
+          `
+    };
+
+    const token = this.context.token;
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    }).then(res => {
+      if(res.status !== 200 && res.status !== 201) {
+        throw new Error('Failed!');
+      }
+      return res.json();
+    })
+    .then(resData => {
+      console.log(resData);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   };
 
   modalCancelHandler = () => {
