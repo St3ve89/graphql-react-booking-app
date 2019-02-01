a894000ee80050815c73c7828df9525247a887d2
Istvan Acs <istvan.acs89@gmail.com>
2019-02-01 13:19:41 +0000

    Finish bookings

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -14,6 +14,8 @@ class EventsPage extends Component {
     selectedEvent: null
   };
 
+  isActive = true;
+
   static contextType = AuthContext;
 
   constructor(props) {
@@ -135,11 +137,15 @@ class EventsPage extends Component {
   })
   .then(resData => {
     const events = resData.data.events;
-    this.setState({events:events, isLoading: false});
+    if(this.isActive) {
+      this.setState({events:events, isLoading: false});
+    }
   })
   .catch(err => {
     console.log(err);
-    this.setState({isLoading: false});
+    if(this.isActive) {
+      this.setState({isLoading: false});
+    }
   });
   }
 
@@ -153,21 +159,19 @@ class EventsPage extends Component {
   bookEventHandler = () => {
     if(!this.context.token) {
       this.setState({selectedEvent: null});
-      return
+      return;
     }
     const requestBody = {
       query: `
-          mutation {
-            bookEvent(eventId: "${this.state.selectedEvent._id}") {
-              _id
-              createdAt
-              updatedAt
-            }
+        mutation {
+          bookEvent(eventId: "${this.state.selectedEvent._id}") {
+            _id
+          createdAt
+          updatedAt
           }
+        }
         `
   };
-
-  const token = this.context.token;
 
   fetch('http://localhost:8000/graphql', {
     method: 'POST',
@@ -176,21 +180,32 @@ class EventsPage extends Component {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + this.context.token
     }
-  }).then(resData => {
+  }).then(res => {
+    if(res.status !== 200 && res.status !== 201) {
+      throw new Error('Failed!');
+    }
+    return res.json();
+  })
+  .then(resData => {
     console.log(resData);
     this.setState({selectedEvent: null});
   })
   .catch(err => {
     console.log(err);
   });
-  };
+};
+
+
+  componentWillUnmount() {
+    this.isActive = false;
+  }
 
   render () {
 
     return (
       <React.Fragment>
         {(this.state.creating || this.state.selectedEvent) && <Backdrop />}
-        {this.state.creating && <Modal title="Add Event" canCancel="true" canConfirm="true" onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler} confirmText={this.context.token ? 'Book' : 'Confirm'}>
+        {this.state.creating && <Modal title="Add Event" canCancel="true" canConfirm="true" onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler} confirmText='Confirm'>
           <form>
             <div className="form-control">
               <label htmlFor="title">title</label>
@@ -210,11 +225,11 @@ class EventsPage extends Component {
             </div>
           </form>
         </Modal>}
-        {this.state.selectedEvent && <Modal title={this.state.selectedEvent.title} canCancel="true" canConfirm="true" onCancel={this.modalCancelHandler} onConfirm={this.bookEventHandler} confirmText="Book">
+        {this.state.selectedEvent && (<Modal title={this.state.selectedEvent.title} canCancel="true" canConfirm="true" onCancel={this.modalCancelHandler} onConfirm={this.bookEventHandler} confirmText={this.context.token ? 'Book' : 'Confirm'}>
           <h1>{this.state.selectedEvent.title}</h1>
           <h2>£{this.state.selectedEvent.price} - {new Date(this.state.selectedEvent.date).toLocaleDateString()}</h2>
           <p>{this.state.selectedEvent.description}</p>
-        </Modal>}}
+        </Modal>)}
         {this.context.token && <div className="events-control">
           <p>Share your own Events!</p>
           <button className="btn" onClick={this.createEventHandler}>Create Event</button>
